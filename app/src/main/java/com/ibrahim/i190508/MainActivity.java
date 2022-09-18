018dff4aff7bd0acc49998f056d18223968e8921
Ibrahim <i190508@nu.edu.pk>
2022-09-18 19:18:23 +0500

    Commit: Twenty First Screen

--- app/src/main/java/com/ibrahim/i190508/MainActivity.java
+++ app/src/main/java/com/ibrahim/i190508/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen22);
+        setContentView(R.layout.screen23);
     }
 }
