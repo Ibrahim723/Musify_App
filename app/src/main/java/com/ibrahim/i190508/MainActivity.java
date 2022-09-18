b1389f6a6e8f3e160b79c710ec70d8765831fcfd
Ibrahim <i190508@nu.edu.pk>
2022-09-18 17:47:25 +0500

    Commit: Nineteenth Screen

--- app/src/main/java/com/ibrahim/i190508/MainActivity.java
+++ app/src/main/java/com/ibrahim/i190508/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen20);
+        setContentView(R.layout.screen21);
     }
 }
