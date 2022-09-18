8fc49ae3ec802c82918bceaee6b454e4cac76d3c
Ibrahim <i190508@nu.edu.pk>
2022-09-18 16:10:53 +0500

    Commit: Fifteenth Screen

--- app/src/main/java/com/ibrahim/i190508/MainActivity.java
+++ app/src/main/java/com/ibrahim/i190508/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen17);
+        setContentView(R.layout.screen18);
     }
 }
